704284a05fa846fd36d2c00b79dcd1972982dea9
kenefu020 <kenefu020@gmail.com>
2025-01-17 20:40:46 +0900

    更新

---
+++ index.js
@@ -0,0 +1,10 @@
+//リスト
+let list = ['大吉','中吉','小吉','吉','末吉','凶','大凶']
+let r = Math.random() * list.length
+let r2 =Math.floor(r)
+console.log(r2)
+console.log(list[r2])
+
+//htmlタグ内に表示させるやつ
+let fin = document.getElementById('fin')
+fin.innerText = list[r2]
